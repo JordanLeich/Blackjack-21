75cd5188b45e130102aa0b450fb2883a64497b5d
Jordan Leich <jordanleich@gmail.com>
2021-09-03 19:38:21 -0400

    Added an extras option to the main menu of the game, updated exe

--- blackjack.py
+++ blackjack.py
@@ -94,7 +94,136 @@ player3_bankrupt = bool
 donate_money = 0
 
 
+def contributions():
+    """
+Allows the user to be able to view all of the contributors of this project via GitHub
+    """
+    print(colors.green + "Opening all contributors of this project...\n", colors.reset)
+    webbrowser.open_new(
+        "https://github.com/JordanLeich/Blackjack-21/graphs/contributors")
+    time.sleep(2)
+
+
+def releases():
+    """
+Allows the user to be able to view either the latest or oldest release of this project via GitHub
+    """
+    while True:
+        choice = int(input('''(1) Latest Stable Release
+(2) Oldest Release
+(3) Return to previous window
+(4) Exit
+
+Which release would you like to view: '''))
+        print()
+
+        if choice == 1:
+            release_opener("Opening the latest stable release...\n",
+                           "https://github.com/JordanLeich/Blackjack-21/releases")
+
+        elif choice == 2:
+            release_opener("Opening the oldest release...\n",
+                           "https://github.com/JordanLeich/Blackjack-21/releases/tag/v5.0")
+
+        elif choice == 3:
+            return
+        elif choice == 4:
+            break
+        else:
+            print(colors.red + 'User input error found...\n', colors.reset)
+            time.sleep(2)
+    print("Reached end of the program... Ending program...\n")
+    exit()
+
+
+def release_opener(arg0, arg1):
+    """
+Used to open a release version in the users default web browser
+    """
+    print(colors.green + arg0, colors.reset)
+    time.sleep(1)
+    webbrowser.open_new(arg1)
+    time.sleep(1)
+
+
+def donate():
+    """
+Allows the user to be able to donate via PayPal or Cash App methods
+    """
+    while True:
+        print(colors.yellow + 'Since this project is completely free to use and open source, users have the option to '
+                              'send a donation of their choice but this action is not required.\n', colors.reset)
+        time.sleep(2)
+        donate_choice = int(input('''(1) PayPal
+(2) Cash App
+(3) Return to previous window
+(4) Exit
+
+Which donation option would you like to use: '''))
+        print()
+
+        if donate_choice == 1:
+            donation_opener("Opening PayPal Donation page...\n",
+                            "https://www.paypal.com/donate/?business=8FGHU8Z4EJPME&no_recurring=0&currency_code=USD")
+
+        elif donate_choice == 2:
+            donation_opener("Opening Cash App Donation page...\n", "https://cash.app/$JordanLeich")
+
+        elif donate_choice == 3:
+            return
+        elif donate_choice == 4:
+            break
+        else:
+            print(colors.red + 'User input error found...\n', colors.reset)
+            time.sleep(2)
+    print("Reached end of the program... Ending program...\n")
+    exit()
+
+
+def donation_opener(arg0, arg1):
+    """
+Used to open a donation page in the users default web browser
+    """
+    print(colors.green + arg0, colors.reset)
+    webbrowser.open_new(arg1)
+    time.sleep(2)
+
+
+def extra():
+    """
+Main hub UI for the user to view additional information or extra parts of this project such as donations and releases
+    """
+    while True:
+        choice = int(input('''(1) Project Releases
+(2) Credits
+(3) Donate
+(4) Return to main window
+(5) Exit
+
+Which option would you like: '''))
+        print()
+
+        if choice == 1:
+            releases()
+        elif choice == 2:
+            contributions()
+        elif choice == 3:
+            donate()
+        elif choice == 4:
+            main()
+        elif choice == 5:
+            break
+        else:
+            print(colors.red + 'User input error found...\n', colors.reset)
+            time.sleep(2)
+    print("Reached end of the program... Ending program...\n")
+    exit()
+
+
 def reset_stats():
+    """
+Used for the end-user to automatically reset their saved stats back to default while in-game
+    """
     global user_balance, dealer_balance, user_score, user_data_file, player1_balance, player2_balance, player3_balance
     green('All players and the dealers money/stats will be reset to their original defaults...')
     time.sleep(1)
@@ -111,7 +240,7 @@ def reset_stats():
     main()
 
 
-def another_game():  # sourcery no-metrics
+def another_game():  # sourcery no-metrics skip: hoist-statement-from-if
     """
 Used when 1 single round of blackjack has ended. Allows the user to play another game of blackjack or quit playing
     """
@@ -512,7 +641,7 @@ def main():
 Used as the first piece of the program introduced to the end-user. This section allows the user to skip around in the
 game by using the game mode selection choices
     """
-    user_knowledge = input('Start / Tutorial / Express / View Stats / Reset Stats / Quit: ')
+    user_knowledge = input('Start / Tutorial / Express / View Stats / Reset Stats / Extras / Quit: ')
     print()
     time.sleep(.5)
 
@@ -527,10 +656,12 @@ game by using the game mode selection choices
         game()
     elif user_knowledge.lower() in ['e', 'express']:
         game()
-    elif user_knowledge.lower() in ['reset', 'reset stats']:
-        reset_stats()
     elif user_knowledge.lower() in ['stats', 'view stats', 'v', 'view']:
         view_stats()
+    elif user_knowledge.lower() in ['reset', 'reset stats']:
+        reset_stats()
+    elif user_knowledge.lower() in ['extra', 'extras']:
+        extra()
     elif user_knowledge.lower() in ['quit', 'q', 'exit']:
         quit()
     else:
