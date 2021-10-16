9e7805bad515c1a849f99dc144422b5eac1ee70d
amastis <37512516+amastis@users.noreply.github.com>
2021-10-16 07:37:18 -0400

    added print statements for import in files

--- other/colors.py
+++ other/colors.py
@@ -3,9 +3,19 @@
 # Imports
 from colored import fg, attr
 
-# Globals
-green = fg('green')
-red = fg('red')
-yellow = fg('yellow')
-blue = fg('blue')
 reset = attr('reset')
+
+def print_green(message_text):
+    print(f'{fg("green")}{message_text}{reset}')
+
+def print_red(message_text):
+    print(f'{fg("red")}{message_text}{reset}')
+
+def print_yellow(message_text):
+    print(f'{fg("yellow")}{message_text}{reset}')
+
+def print_blue(message_text):
+    print(f'{fg("blue")}{message_text}{reset}')
+
+def user_error(message_text):
+    print_red(message_text)
